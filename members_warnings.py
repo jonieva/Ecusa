86aa3d8f53f1b5a134430ba12ef8f76cd9337bd7
jonieva <jorgeonieva@gmail.com>
2016-09-25 12:08:29 -0400

    BUG: encoding issues and additional options for members expired warnings

--- members_warnings.py
+++ members_warnings.py
@@ -1,6 +1,6 @@
 import argparse
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from oauth2client.service_account import ServiceAccountCredentials      # pip install oauth2client
 import smtplib
 import datetime
 
@@ -21,12 +21,21 @@ from oauth2client.file import Storage
 from apiclient import discovery
 
 credentials_file = "Ecusa-credentials-secretary.json"
+fromaddr = 'secretary-bos@ecusa.es'
+
 
 def get(row, letter):
-    return row[ord(letter) - ord('A')].decode("utf-8")
+    return row[ord(letter) - ord('A')]
 
 
-def members_about_to_expire_warning(warn_about_members_expired=True):
+def members_about_to_expire_warning(destination, first_reminder_timespan_days=7, expired_for_at_least_timespan_days=0):
+    """
+    Send an email warning about the members that are about to expire and the members that already expired
+    :param destination: list od email addresses
+    :param first_reminder_timespan_days: user is going to expire in X days
+    :param expired_for_at_least_timespan_days: user has been expired for at least X days
+    :return:
+    """
     scope = ['https://spreadsheets.google.com/feeds']
     credentials = ServiceAccountCredentials.from_json_keyfile_name(credentials_file, scope)
     gc = gspread.authorize(credentials)
@@ -40,18 +49,20 @@ def members_about_to_expire_warning(warn_about_members_expired=True):
 
     date = get(row, 'M')
 
-    first_reminder_timespan_days = -4
-    second_reminder_timespan_days = 0
     today = datetime.datetime.today().date()
     members_about_to_expire = []
     members_expired = [""]
 
+    if expired_for_at_least_timespan_days > 0:
+        expired_for_at_least_timespan_days = -expired_for_at_least_timespan_days
+
     while date and i < row_count:
+        print ("Processing {}".format(i))
         d = datetime.datetime.strptime(date, "%m/%d/%Y %H:%M:%S").date()
         if (today-d).days == 365-first_reminder_timespan_days:
             members_about_to_expire.append(u"Name: {};\nDate: {};\nEmail: {}\nRow number: {}\n\n".
                                            format(get(row, 'P'), get(row, 'M'), get(row, 'T'), i))
-        elif (today-d).days >= 365-second_reminder_timespan_days:
+        elif (today-d).days >= 365-expired_for_at_least_timespan_days:
             members_expired.append(u"Name: {};\nDate: {};\nEmail: {}\nRow number: {}\n\n".
                                            format(get(row, 'P'), get(row, 'M'), get(row, 'T'), i))
         i += 1
@@ -59,8 +70,6 @@ def members_about_to_expire_warning(warn_about_members_expired=True):
         date = get(row, 'M')
 
     if not members_about_to_expire and not members_expired:
-        return
-    if not members_about_to_expire and not warn_about_members_expired:
         return
 
     message_text = "This is an automatic notification for ECUSA secretary.\n\n"
@@ -72,11 +81,10 @@ def members_about_to_expire_warning(warn_about_members_expired=True):
         message_text += "\nMembers EXPIRED:\n\n"
         message_text += "\n".join(members_expired)
 
-    send_email("jorgeonieva@gmail.com", "ECUSA users notification", message_text)
+    send_email(destination, "ECUSA users notification", message_text)
 
 
 def send_email(toaddrs, subject, body):
-    fromaddr = 'secretary-bos@ecusa.es'
     with open("mail_pwd.txt", 'r') as f:
         password=f.readline()
 
@@ -85,13 +93,13 @@ def send_email(toaddrs, subject, body):
     server.starttls()
     server.login(fromaddr, password)
 
-    msg = "\r\n".join([
-        "From: {}",
+    msg = u"\r\n".join([
+        "From: ECUSA automatic tools",
         "To: {}",
         "Subject: {}",
         "",
         "{}"
-    ]).format(fromaddr, toaddrs, subject, body)
+    ]).format(toaddrs, subject, body).encode("utf8")
 
     server.sendmail(fromaddr, toaddrs, msg)
     server.quit()
@@ -99,11 +107,22 @@ def send_email(toaddrs, subject, body):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='ECUSA notifications')
-    parser.add_argument('--warn_expired_users', type=bool,
-                        help="When False, there won't be a notification if we have only expired users", default=True)
+    # parser.add_argument('--warn_expired_users', type=bool,
+    #                     help="When False, there won't be a notification if we have only expired users", default=True)
+    parser.add_argument('--destination', type=str, required=True,
+                        help="Email address of the destination (if more than one, separate them with ;)")
+    parser.add_argument('--first_reminder', type=int, default=7,
+                        help="A reminder of the users that are going to expire in X days",
+                        )
+    parser.add_argument('--expired_for_at_least', type=int, default=0,
+                        help="Replace users considered expired. For example, if expired_for_at_least = 7, only warn "
+                             "about the users that have been expired for more than 7 days")
+    expired_for_at_least_timespan_days = 0
 
     args = parser.parse_args()
-    members_about_to_expire_warning(args.warn_expired_users)
+    dest = map(str.strip, args.destination.split(';'))
+    members_about_to_expire_warning(dest, first_reminder_timespan_days=args.first_reminder,
+                                    expired_for_at_least_timespan_days=args.expired_for_at_least)
 
 
 
